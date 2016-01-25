bf1dbc0bf00a512f96c1eb99a5da6c26e2a8b72b
ironmaniiith <aalekhj2507@gmail.com>
2016-01-26 00:39:48 +0530

    added directory to download files to

--- DownloadAudio.py
+++ DownloadAudio.py
@@ -3,6 +3,10 @@
 import re
 import os
 
+__DIR__ = './songs/' # Enter the directory in which you want to download the songs
+if not os.path.exists(__DIR__):
+    os.makedirs(__DIR__)
+
 p= re.compile('https:*')
 FilePointer = open("README.md")
 lines=[]
@@ -13,6 +17,5 @@ for i in lines:
     m = p.search(i)
     if(m is not None):
         url = i[m.start():-1] # -1 to remove the ')' present in end of line 
-        #print url
-        args = "--extract-audio --audio-format mp3 --prefer-ffmpeg -w "+url
+        args = '-o "songs/%(title)s.%(ext)s" --extract-audio --audio-format mp3 --prefer-ffmpeg -w ' + url
         os.system("youtube-dl "+args)
