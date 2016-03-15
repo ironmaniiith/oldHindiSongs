8d1f17aec321d83def7ad3a895b73fcc52cd74c6
ironmaniiith <aalekhj2507@gmail.com>
2016-03-15 20:47:37 +0000

    Init commit

---
+++ DownloadAudio.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+#Made to ease the download process. No copyright infringement intended.
+import re
+import os
+
+__DIR__ = './songs/' # Enter the directory in which you want to download the songs, make sure to give '/' at the end
+if not os.path.exists(__DIR__):
+    os.makedirs(__DIR__)
+
+p= re.compile('https:*')
+FilePointer = open("README.md")
+lines=[]
+for i in FilePointer.readlines():
+    lines.append(i[:-1]) # -1 to remove the '\n' that comes along
+
+for i in lines:
+    m = p.search(i)
+    if(m is not None):
+        url = i[m.start():-1] # -1 to remove the ')' present in end of line 
+        args = '-o "' + __DIR__ + '%(title)s.%(ext)s" --extract-audio --audio-format mp3 --prefer-ffmpeg -w ' + url
+        os.system("youtube-dl "+args)
---
+++ README.md
@@ -0,0 +1,1 @@
+Collection of old hindi evergreen songs that I just can't stop listening.
